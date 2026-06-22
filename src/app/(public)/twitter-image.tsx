88d0c97ecd34da107ce7713f9e15ee499c52047e
Raziel Gershoni <zhendos13@gmail.com>
2026-06-22 09:26:12 +0300

    Revert "feat(site): generated OG/Twitter card with bundled Hebrew font"
    This reverts commit 1c4a43138bfd97548f08c226a90b7b0e9478df61.

--- src/app/(public)/twitter-image.tsx
+++
@@ -1,8 +0,0 @@
-// Twitter/X large-summary card reuses the same generated DOCKET card.
-export { default } from './opengraph-image';
-
-export const runtime = 'nodejs';
-export const revalidate = 3600;
-export const size = { width: 1200, height: 630 };
-export const contentType = 'image/png';
-export const alt = 'מאפיית מחמצת';
